5d797d73e9aafe8eda7ca8f0a42f20568e7f7d28
say-annyeong <sinjemen07@gmail.com>
2025-02-13 17:12:39 +0900

    proc_macro test

--- src/piece.rs
+++ src/piece.rs
@@ -5,39 +5,53 @@ use std::{
         Formatter
     },
     collections::HashMap,
-    sync::Arc,
+    sync::{
+        Arc,
+    },
+    any::Any
 };
-use rayon::prelude::*;
+use rayon::prelude::{
+    ParallelIterator,
+    IntoParallelIterator
+};
+use regex::Regex;
+use lazy_static::lazy_static;
+use crate::BOARD_X_SIZE;
 
 // 2차원 벡터
 pub type VecXY<T> = Vec<Vec<T>>;
 
+lazy_static! {
+    static ref PLAYER_INPUT_RE: Regex = Regex::new(
+        r"(?P<name>[A-Za-z]*)(?P<start_col>[A-Za-z]*)(?P<start_row>\d*)(?P<takes>[Xx]?)(?P<end_col>[A-Za-z]+)(?P<end_row>\d+)(?P<other>.*)"
+    ).unwrap();
+}
+
 /// 보드 정보를 위한 구조체.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Board {
     color: Option<String>,
     piece_type: Option<String>,
-    // 빈 벡터 던지면 아무튼 None임
-    other: Vec<String>
+    other: Option<Vec<String>>
 }
 
 impl Board {
-    pub fn new(color: Option<String>, piece_type: Option<String>, other: Vec<String>) -> Self {
+    pub fn new(color: Option<String>, piece_type: Option<String>, other: Option<Vec<String>>) -> Self {
         Self { color, piece_type, other }
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        return match (&self.color, &self.piece_type, &self.other.is_empty()) {
-            (Some(color), None, true) =>                      write!(f, "{}", color),
-            (None, Some(piece_type), true) =>                 write!(f, "{}", piece_type),
-            (Some(color), Some(piece_type), true) =>  write!(f, "{}{}", color, piece_type),
-            (None, None, false) =>                                    write!(f, "{:?}", self.other),
-            (Some(color), None, false) =>                     write!(f, "{}{:?}", color, self.other),
-            (None, Some(piece_type), false) =>                write!(f, "{}{:?}", piece_type, self.other),
-            (Some(color), Some(piece_type), false) => write!(f, "{}{}{:?}", color, piece_type, self.other),
-            _ =>                                                      write!(f, "None")
+        match (&self.color, &self.piece_type, &self.other) {
+            (Some(color), None, None) => write!(f, "{}", color),
+            (None, Some(piece_type), None) => write!(f, "{}", piece_type),
+            (Some(color), Some(piece_type), None) => write!(f, "{}{}", color, piece_type),
+            (None, None, Some(other)) => write!(f, "{:?}", other),
+            (Some(color), None, Some(other)) => write!(f, "{}{:?}", color, other),
+            (None, Some(piece_type), Some(other)) => write!(f, "{}{:?}", piece_type, other),
+            (Some(color), Some(piece_type), Some(other)) => write!(f, "{}{}{:?}", color, piece_type, other),
+            (None, None, None) => write!(f, "None")
         }
     }
 }
@@ -65,34 +79,35 @@ impl Display for Board {
 /// // (0, 0)에서 출발하며, (1, 1)로 이동이 가능하며, 이동하는 속성을 가진다. 기물의 색상과 종류는 백색 비숍이다. 이동과 잡기가 가능하다.
 /// ```
 ///
-#[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
+#[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug, Default)]
 pub struct MoveType {
-    pub cx: Option<usize>,
-    pub cy: Option<usize>,
-    pub x: Option<usize>,
-    pub y: Option<usize>,
-    pub move_type: Option<String>,
-    pub piece_type: Option<String>,
-    pub color: Option<String>,
-    pub takes_color: Option<String>,
-    pub takes_piece_type: Option<String>,
-    // 빈 벡터 던지면 아무튼 None임
-    pub other: Vec<String>
+    cx: Option<usize>,
+    cy: Option<usize>,
+    x: Option<usize>,
+    y: Option<usize>,
+    move_type: Option<String>,
+    piece_type: Option<String>,
+    color: Option<String>,
+    takes_color: Option<String>,
+    takes_piece_type: Option<String>,
+    other: Option<Vec<String>>
 }
 
 impl MoveType {
     pub fn new(cx: Option<usize>, cy: Option<usize>, x: Option<usize>, y: Option<usize>,
                       move_type: Option<String>, piece_type: Option<String>, color: Option<String>,
-                      takes_color: Option<String>, takes_piece_type: Option<String>, other: Vec<String>) -> Self {
+                      takes_color: Option<String>, takes_piece_type: Option<String>, other: Option<Vec<String>>) -> Self {
         Self { cx, cy, x, y, move_type, piece_type, color, takes_color, takes_piece_type, other }
-    }
-    
-    pub fn none() -> Self {
-        Self { cx: None, cy: None, x: None, y: None, move_type: None, piece_type: None, color: None, takes_color: None, takes_piece_type: None, other: Vec::new() }
     }
 
     fn all_none(&self) -> bool {
         self.cx == None && self.cy == None && self.x == None && self.y == None && self.move_type == None && self.piece_type == None && self.color == None && self.takes_color == None && self.takes_piece_type == None
+    }
+    
+    fn set_other(input: Option<Vec<String>>) -> Self {
+        let mut move_type = Self::default();
+        move_type.other = input;
+        move_type
     }
 }
 
@@ -161,20 +176,20 @@ impl<'a> CalculateMoves<'a> {
     fn step(&self, x: usize, y: usize, walk_type: WalkType) -> MoveType {
         if self.board.len() <= x || self.board[0].len() <= y {
             // 보드 범위 초과.
-            MoveType::new(None, None, None, None, None, None, None, None, None, Vec::new())
+            MoveType::default()
         }
         else {
             match (&self.board[x][y].color, &self.board[x][y].piece_type) {
                 // 이동 가능.
                 (None, None) if walk_type.other.contains(&"move".to_string()) => {
-                    MoveType::new(None, None, Some(x), Some(y), Some("m".to_string()), Some(walk_type.piece_type), Some(walk_type.color), None, None, walk_type.other.clone())
+                    MoveType::new(None, None, Some(x), Some(y), Some("m".to_string()), Some(walk_type.piece_type), Some(walk_type.color), None, None, Some(walk_type.other))
                 },
                 // 그 위치에 잡을 수 있는 기물 있음.
                 (Some(color), Some(piece_type)) if color != &walk_type.color && walk_type.other.contains(&"capture".to_string()) => {
-                    MoveType::new(None, None, Some(x), Some(y), Some("x".to_string()), Some(walk_type.piece_type), Some(walk_type.color), Some(color.clone()), Some(piece_type.clone()), walk_type.other.clone())
+                    MoveType::new(None, None, Some(x), Some(y), Some("x".to_string()), Some(walk_type.piece_type), Some(walk_type.color), Some(color.clone()), Some(piece_type.clone()), Some(walk_type.other))
                 }
                 // 이동 불가.
-                _ => MoveType::new(None, None, None, None, None, None, None, None, None, Vec::new())
+                _ => MoveType::default()
             }
         }
     }
@@ -197,8 +212,17 @@ impl<'a> CalculateMoves<'a> {
             if cx != x || cy != y {
                 let mut moving = self.step(x, y, walk_type.clone());
                 match moving.all_none() {
-                    true if moving.other.contains(&"jump_1".to_string()) && jump == 0 => continue,
-                    true => break,
+                    true => {
+                        if let Some(other) = moving.other {
+                            if other.contains(&"jump_1".to_string()) && jump == 0 {
+                                continue
+                            } else { 
+                                break
+                            }
+                        } else { 
+                            break
+                        }
+                    },
                     false => {
                         (moving.cx, moving.cy) = (Some(cx), Some(cy));
                         moves.push(moving.clone());
@@ -268,7 +292,7 @@ impl<'a> CalculateMoves<'a> {
             (move_type.cx, move_type.cy, move_type.x, move_type.y)
         {
             buffer[x][y] = buffer[cx][cy].clone();
-            buffer[cx][cy] = Board::new(None, None, Vec::new());
+            buffer[cx][cy] = Board::new(None, None, None);
             buffer
         } else {
             buffer
@@ -294,7 +318,7 @@ impl MainCalculate {
             (move_type.cx, move_type.cy, move_type.x, move_type.y)
         {
             (*self.board)[x][y] = self.board[cx][cy].clone();
-            self.board[cx][cy] = Board::new(None, None, Vec::new());
+            self.board[cx][cy] = Board::default();
         }
     }
 
@@ -302,9 +326,14 @@ impl MainCalculate {
         CalculateMoves::new(self.board.clone(), &self.piece_type, &self.piece_direction).piece_moved(move_type)
     }
 
-    pub fn calculate_moves(&mut self, deep: usize) {
+    pub fn calculate_move(&mut self, deep: usize) {
         let calculate = Arc::new(CalculateMoves::new(self.board.clone(), &self.piece_type, &self.piece_direction));
         self.save_moves = calculate.search_piece(deep);
+    }
+    
+    pub fn calculate_moved(&self, deep: usize) -> CanMove {
+        let calculate = Arc::new(CalculateMoves::new(self.board.clone(), &self.piece_type, &self.piece_direction));
+        calculate.search_piece(deep)
     }
 
     pub fn continue_calculate_moves(&mut self, insert_can_move: &mut CanMove) {
@@ -318,30 +347,46 @@ impl Default for MainCalculate {
     }
 }
 
-/// 수 추적을 위한 열거형
-///
+/// 수 추적 및 통신을 위한 열거형
+/// 
+/// 이 열거형은 게임 상태를 추적하고, 수의 연쇄적 진행을 관리하는 데 사용됩니다.
+/// 
 /// 필드 설명:
-/// - CanMoves: 수 추적 연쇄형. 빈 해시맵으로 추적 일시 중단 가능.
-/// - Board: 수 추적 명시적 종료.
-/// - None: 임시 종료 필드
-#[derive(Clone, Debug)]
+/// - `CanMoves`: 수 추적의 트리 구조. 이 변형은 가능한 모든 이동들을 추적하는 해시맵을 포함하고 있으며,
+///   빈 해시맵을 사용하여 수 추적을 일시적으로 중단할 수 있습니다.
+///   이 경우 해시맵에 수 추척 결과를 계속 담음으로써 추척을 계속합니다.
+///   추적이 계속 진행될 때마다 새로운 이동들이 추가될 수 있습니다.
+///     - 'VecXY<Board>': 현재 보드 상태.
+///     - 'HashMap<MoveType, Box<Self>>': 현재 보드 상태에 MoveType이 적용된 상태를 Box<Self>에 담습니다.
+/// - `Board`: 수 추적이 명시적으로 종료된 상태를 나타냅니다. 이 변형은 게임 보드 상태를 포함하며,
+///   수 추적이 완료되었음을 나타냅니다.
+/// - `None`: 기본값을 나타낼 때 사용됩니다. 기본값을 설정할 때 사용됩니다.
+#[derive(Clone, Debug, Default)]
 pub enum CanMove {
-    CanMoves((VecXY<Board>, HashMap<MoveType, Box<CanMove>>)),
+    CanMoves((VecXY<Board>, HashMap<MoveType, Box<Self>>)),
     Board(VecXY<Board>),
-    None
+    #[default] None
 }
 
 impl CanMove {
+    pub fn as_can_moves(&self) -> Option<&(VecXY<Board>, HashMap<MoveType, Box<Self>>)> {
+        match self {
+            Self::CanMoves(moves) => Some(moves),
+            _ => None
+        }
+    }
+    
     pub fn as_board(&self) -> Option<&VecXY<Board>> {
         match self {
-            CanMove::Board(board) => Some(board),
+            Self::Board(board) => Some(board),
             _ => None,
         }
     }
 
-    pub fn as_can_moves(&self) -> Option<&(VecXY<Board>, HashMap<MoveType, Box<CanMove>>)> {
-        match self {
-            CanMove::CanMoves(move_type) => Some(move_type),
+    pub fn as_value(&self) -> Option<&dyn Any> {
+        match self { 
+            Self::CanMoves(moves) => Some(moves),
+            Self::Board(board) => Some(board),
             _ => None
         }
     }
@@ -350,30 +395,30 @@ impl CanMove {
 pub fn default_board() -> VecXY<Board> {
     vec![
         vec![
-            Board::new(Some("black".to_string()), Some("rook".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("knight".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("bishop".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("queen".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("king".to_string()), vec!["move".to_string(), "capture".to_string(), "check".to_string(), "threatened".to_string(), "checkmate".to_string()]),
-            Board::new(Some("black".to_string()), Some("bishop".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("knight".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("black".to_string()), Some("rook".to_string()), vec!["move".to_string(), "capture".to_string()])
+            Board::new(Some("black".to_string()), Some("rook".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("knight".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("bishop".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("queen".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("king".to_string()), Some(vec!["move".to_string(), "capture".to_string(), "check".to_string(), "threatened".to_string(), "checkmate".to_string()])),
+            Board::new(Some("black".to_string()), Some("bishop".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("knight".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("black".to_string()), Some("rook".to_string()), Some(vec!["move".to_string(), "capture".to_string()]))
         ],
-        vec![Board::new(Some("black".to_string()), Some("pawn".to_string()), vec!["move".to_string(), "capture".to_string(), "promotion".to_string()]); 8],
-        vec![Board::new(None, None, Vec::new()); 8],
-        vec![Board::new(None, None, Vec::new()); 8],
-        vec![Board::new(None, None, Vec::new()); 8],
-        vec![Board::new(None, None, Vec::new()); 8],
-        vec![Board::new(Some("white".to_string()), Some("pawn".to_string()), vec!["move".to_string(), "capture".to_string(), "promotion".to_string()]); 8],
+        vec![Board::new(Some("black".to_string()), Some("pawn".to_string()), Some(vec!["move".to_string(), "capture".to_string(), "promotion".to_string()])); 8],
+        vec![Board::default(); 8],
+        vec![Board::default(); 8],
+        vec![Board::default(); 8],
+        vec![Board::default(); 8],
+        vec![Board::new(Some("white".to_string()), Some("pawn".to_string()), Some(vec!["move".to_string(), "capture".to_string(), "promotion".to_string()])); 8],
         vec![
-            Board::new(Some("white".to_string()), Some("rook".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("knight".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("bishop".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("queen".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("king".to_string()), vec!["move".to_string(), "capture".to_string(), "check".to_string(), "threatened".to_string(), "checkmate".to_string()]),
-            Board::new(Some("white".to_string()), Some("bishop".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("knight".to_string()), vec!["move".to_string(), "capture".to_string()]),
-            Board::new(Some("white".to_string()), Some("rook".to_string()), vec!["move".to_string(), "capture".to_string()])
+            Board::new(Some("white".to_string()), Some("rook".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("knight".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("bishop".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("queen".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("king".to_string()), Some(vec!["move".to_string(), "capture".to_string(), "check".to_string(), "threatened".to_string(), "checkmate".to_string()])),
+            Board::new(Some("white".to_string()), Some("bishop".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("knight".to_string()), Some(vec!["move".to_string(), "capture".to_string()])),
+            Board::new(Some("white".to_string()), Some("rook".to_string()), Some(vec!["move".to_string(), "capture".to_string()]))
         ],
     ]
 }
@@ -475,4 +520,53 @@ pub fn default_piece_move() -> Vec<WalkType> {
 
 pub fn default_setting() -> (VecXY<Board>, Vec<String>, Vec<WalkType>) {
     (default_board(), default_piece_type(), default_piece_move())
+}
+
+fn custom_calculate_moved(board: VecXY<Board>, piece_type: Vec<String>, piece_direction: Vec<WalkType>, deep: usize) -> CanMove {
+    MainCalculate::new(board, piece_type, piece_direction).calculate_moved(deep)
+}
+
+fn chess_x_convent(input: String) -> usize {
+    let input_parse = input.trim().parse::<usize>().unwrap();
+    let index: Vec<usize> = unsafe { (0..BOARD_X_SIZE).rev().collect() };
+    index[input_parse - 1]
+}
+
+fn chess_y_convent(input: String) -> usize {
+    input.chars().enumerate().map(|(radix, num)| (num.to_digit(36).unwrap() - 9) as usize * 26_usize.pow(radix as u32)).sum::<usize>() - 1
+}
+
+pub fn check_move(moves: Vec<&MoveType>, player_input: String) -> Option<Vec<MoveType>> {
+    if let Some(input) = PLAYER_INPUT_RE.captures(player_input.as_str()) {
+        let (mut name, start_col, start_row, takes, end_col, end_row, other) = (input["name"].to_lowercase(), input["start_col"].to_lowercase(), input["start_row"].to_string(), !input["takes"].is_empty(), input["end_col"].to_lowercase(), input["end_row"].to_string(), input["other"].to_lowercase());
+        let cx = if start_col.is_empty() { None } else { Some(chess_y_convent(start_col)) };
+        let cy = if start_row.is_empty() { None } else { Some(chess_x_convent(start_row)) };
+        let x = chess_x_convent(end_row);
+        let y = chess_y_convent(end_col);
+
+        if name.is_empty() {
+            name = "pawn".to_string();
+        }
+
+        let mut can_moves = Vec::new();
+        for move_type in moves {
+            let name_correct = Some(name.clone()) == move_type.piece_type;
+            let start_col_correct = cx == move_type.cx || cx.is_none();
+            let start_row_correct = cy == move_type.cy || cy.is_none();
+            //let takes_correct = if takes { Some("x".to_string()) } else { None } == move_type.move_type;
+            let end_col_correct = Some(x) == move_type.x;
+            let end_row_correct = Some(y) == move_type.y;
+            if name_correct && start_col_correct && start_row_correct && end_col_correct && end_row_correct {
+                can_moves.push(move_type);
+            }
+        }
+        
+        Some(can_moves.into_iter().map(|move_type| move_type.clone()).collect())
+    } else {
+        Some(vec![MoveType::set_other(Some(vec![player_input]))])
+    }
+}
+
+fn custom_check_move(board: VecXY<Board>, piece_type: Vec<String>, piece_move: Vec<WalkType>, player_input: String) -> Option<Vec<MoveType>> {
+    check_move(custom_calculate_moved(board, piece_type, piece_move, 1).as_can_moves().unwrap().1.keys().collect(), player_input)
 }
